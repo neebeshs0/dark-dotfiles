a7789ebfd96d120b4ec529b651df94fe000fc3a9
Neebeshs0 <neebeshs0@github.com>
2024-05-26 09:35:11 +0545

    Added Nepali Date to Calendar

--- ags/widgets/bar/modules/Clock.js
+++ ags/widgets/bar/modules/Clock.js
@@ -1,89 +1,105 @@
-const time = Variable("", {
-    poll: [1000, 'date +"%l:%M %p"'],
+
+const time = Variable('', {
+  poll: [1000, 'date +"%l:%M %p"'],
 })
 
-const date = Variable("", {
-    poll: [1000, 'date +"%d %b %Y"'],
+const date = Variable('', {
+  poll: [1000, 'date +"%d %b %Y"'],
 })
 
 const day = Variable('', {
-    poll: [1000, 'date +"%A%n"']
+  poll: [1000, 'date +"%A%n"']
+})
+
+const nepaliDate = Variable('', {
+  poll: [
+    1000,
+    ['bash', '-c', 'node ~/.config/ags/widgets/utils/GetNepaliDate.js']
+  ]
 })
 
 const toottipDayDate = Variable('', {
-    poll: [1000, 'date +"%A%n%d %b %Y"']
+  poll: [1000, 'date +"%A%n%d %b %Y"']
 })
 
+
 function getDateAndTime() {
-    return Widget.Box({
-        class_name: 'date-time',
-        vertical: true,
-        children: [
-            Widget.Label({
-                class_name: 'day',
-                label: day.bind(),
-            }),
-            Widget.Label({
-                class_name: 'date',
-                label: date.bind(),
-            }),
-        ]
-    })
+  return Widget.Box({
+    vpack: 'center',
+    class_name: 'date-time',
+    vertical: true,
+    children: [
+      Widget.Label({
+        class_name: 'day',
+        label: day.bind(),
+      }),
+      Widget.Label({
+        class_name: 'date',
+        label: date.bind(),
+      }),
+      Widget.Label({
+        class_name: 'date',
+        css: 'color: #9ccfd8;',
+        label: nepaliDate.bind()
+      }),
+    ]
+  })
 }
 
+
 function CalendarContainer() {
-    return Widget.Box({
-        class_name: 'calendar-container',
-        visible: false,
-        children: [
-            Widget.Box({
-                homogeneous: true,
-                vertical: true,
-                child: getDateAndTime()
-            }),
-            Widget.Calendar({
-                class_name: 'calendar',
-                showDayNames: true,
-                showDetails: true,
-                showHeading: true,
-            })
-        ]
-    })
+  return Widget.Box({
+    class_name: 'calendar-container',
+    visible: false,
+    children: [
+      Widget.Box({
+        homogeneous: true,
+        vertical: true,
+        child: getDateAndTime()
+      }),
+      Widget.Calendar({
+        class_name: 'calendar',
+        showDayNames: true,
+        showDetails: true,
+        showHeading: true,
+      })
+    ]
+  })
 }
 
 function CalendarRevealer() {
-    return Widget.Revealer({
-        transition: "slide_down",
-        reveal_child: true,
-        child: CalendarContainer(),
-    }).hook(App, (self, wname, visible) => {
-        if (wname === 'calendar')
-            self.reveal_child = visible
-    }, 'window-toggled')
+  return Widget.Revealer({
+    transition: "slide_down",
+    reveal_child: true,
+    child: CalendarContainer(),
+  }).hook(App, (self, wname, visible) => {
+    if (wname === 'calendar')
+      self.reveal_child = visible
+  }, 'window-toggled')
 }
 
 export function CalendarWindow() {
-    return Widget.Window({
-        name: 'calendar',
-        class_name: 'calendar-window',
-        css: 'padding: 1px;',
-        anchor: ['top', 'left'],
-        margins: [12, 12, 0],
-        visible: false,
-        child: Widget.Box({
-            child: CalendarRevealer()
-        })
+  return Widget.Window({
+    name: 'calendar',
+    class_name: 'calendar-window',
+    css: 'padding: 1px;',
+    anchor: ['top', 'left'],
+    margins: [12, 12, 0],
+    visible: false,
+    child: Widget.Box({
+      child: CalendarRevealer()
     })
+  })
 }
 
 export default function Clock() {
-    return Widget.Button({
-        class_name: 'bar-clock',
-        hpack: 'end',
-        onClicked: () => App.toggleWindow('calendar'),
-        tooltipText: toottipDayDate.bind(),
-        child: Widget.Label({
-            label: time.bind(),
-        })
+  return Widget.Button({
+    class_name: 'bar-clock',
+    hpack: 'end',
+    onClicked: () => App.toggleWindow('calendar'),
+    tooltipText: toottipDayDate.bind(),
+    child: Widget.Label({
+      label: time.bind(),
     })
+  })
 }
---
+++ ags/widgets/utils/GetNepaliDate.js
@@ -0,0 +1,4 @@
+import NepaliDate from "../../node_modules/nepali-datetime/dist/index.mjs"
+
+const date = new NepaliDate()
+console.log(date.formatNepali('DD MMMM YYYY'))
